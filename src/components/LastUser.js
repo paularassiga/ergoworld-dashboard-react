463f8a9475e58a9a562bbd8a6b027502f4dbfb30
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2021-05-22 19:12:20 -0300

    se agrega ultimo usuario, no funciona nombre de rol

--- src/components/LastUser.js
+++ src/components/LastUser.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, {
+    useState,
+    useEffect,
+    Component
+} from 'react';
 import imagenFondo from '../assets/images/base-laptop.jpg';
 
-function LastProduct(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Último usuario creado</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle del usuario</a>
-                </div>
-            </div>
-        </div>
-    )
-}
+class LastUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lastUser: []
+        }
+    }
+    componentDidMount() {
+        fetch(`/api/users/lastCreatedUser`, ).then(x => {
+            return x.json()
+        }).then(lastUser => {
+            console.log(lastUser)
+            this.setState({
+                lastUser: lastUser.users[0]
+            })
+        }).catch(error => console.log(error))
+    }
 
-export default LastProduct;
+
+
+
+
+
+render(){
+    return ( 
+    
+    <div className = "col-lg-6 mb-4" >
+        <div className = "card shadow mb-4" >
+        <div className = "card-header py-3" >
+        <h5 className = "m-0 font-weight-bold text-gray-800" > Último usuario creado: {this.state.lastUser.name} 
+        
+        </h5> </div> <div className = "card-body" >
+        <div className = "text-center" >
+        <img className = "img-fluid px-3 px-sm-4 mt-3 mb-4"
+        style = {
+            {
+                width: 40 + 'rem'
+            }
+        }
+        src = {
+            this.state.lastUser.image
+        }
+        alt = " Star Wars - Mandalorian " / >
+        </div> <p > Email: {this.state.lastUser.email} </p>  <a className = "btn btn-danger"
+        target = "_blank"
+        rel = "nofollow"
+        href = {this.state.lastUser.detail}> Ver detalle del usuario < /a> </div> </div> </div>
+    )
+}}
+
+export default LastUser;
